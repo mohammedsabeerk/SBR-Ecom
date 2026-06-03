f35b18230e4791a8838be6c1bc912af9098d4e0a
mohammedsabeerk <mohammedsabeerk06@gmail.com>
2026-06-03 11:37:32 +0530

    fixed api bugs

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,11 +1,11 @@
 import api from "./axios";
 
 export const getProducts = async () => {
-  const res = await api.get("/products");
+  const res = await api.get("/api/products");
   return res.data;
 };
 
 export const getProductById = async (id) => {
-  const res = await api.get(`/products/${id}`);
+  const res = await api.get(`/api/products/${id}`);
   return res.data;
 };
